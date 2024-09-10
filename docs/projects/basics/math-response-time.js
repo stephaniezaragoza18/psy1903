4f30309770c8f870fc8c4dc957ae1b49521f09aa
Stephanie Zaragoza <szaragoza@college.harvard.edu>
2024-09-10 17:38:52 -0400

    math excersie pt.a: added 3 trials and alert messages

---
+++ docs/projects/basics/math-response-time.js
@@ -0,0 +1,27 @@
+//alert('In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.');
+let trial1 = prompt('What is 2 + 2');
+console.log(trial1);
+alert('You answered ' + trial1);
+
+let trial2 = prompt('What is 5 + 1');
+console.log(trial2);
+alert('You answered ' + trial2);
+
+let trial3 = prompt('What is 1 + 1');
+console.log(trial3);
+alert('You answered ' + trial3);
+
+alert("Thank you for your participation!");
+
+
+
+
+
+
+
+
+
+
+
+
+
