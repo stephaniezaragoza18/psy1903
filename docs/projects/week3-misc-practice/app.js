eaf16711c1753feb3115a55c340b787c26dcd0fe
Stephanie Zaragoza <szaragoza@college.harvard.edu>
2024-09-17 17:21:47 -0400

    Misc Practice Part A

---
+++ docs/projects/week3-misc-practice/app.js
@@ -0,0 +1,21 @@
+//let response = prompt('What is 4 + 6?');
+//let response = prompt('What is ' + (Math.floor(Math.random() * 10) + 1) + ' + ' + (Math.floor(Math.random() * 10) + 1) + ' ? ');
+
+let num1 = Math.floor(Math.random() * 10) + 1;
+let num2 = Math.floor(Math.random() * 10) + 1;
+
+let response = prompt('What is ' + num1 + '+' + num2 + '?');
+
+let answer = num1 + num2;
+let feedback = '';
+if (response == answer) {
+    feedback = 'Correct!';
+} else if (response == answer - 1 || response == answer + 1) {
+    feedback = 'You were close!';
+} else {
+    feedback = 'Incorrect.';
+}
+
+alert(feedback + ' The expected answer is ' + answer);
+
+
