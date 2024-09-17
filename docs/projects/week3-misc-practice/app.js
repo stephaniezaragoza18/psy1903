555759645d04dcdea2f12137240a0ebdc7c10989
Stephanie Zaragoza <szaragoza@college.harvard.edu>
2024-09-17 17:49:15 -0400

    Math with Feedback

--- docs/projects/week3-misc-practice/app.js
+++ docs/projects/week3-misc-practice/app.js
@@ -18,16 +18,39 @@
 
 // alert(feedback + ' The expected answer is ' + answer);
 
-let age = prompt('How old are you?');
-if (age < 12) {
-    alert('Child');
-}
-if (age >= 12 && age < 18) {
-    alert('Teenager');
-}
-if (age >= 18) {
-    alert('Adult');
-}
+// let age = prompt('How old are you?');
+// if (age < 12) {
+//     alert('Child');
+// }
+// if (age >= 12 && age < 18) {
+//     alert('Teenager');
+// }
+// if (age >= 18) {
+//     alert('Adult');
+// }
 
+// Create variables to store references to elements on the page
+let form = document.getElementsByTagName('form')[0];
+let results = document.getElementById('results');
 
+// Listen for the form to be submitted
+form.addEventListener('submit', function (event) {
 
+    // Prevent the default form submission b
+    event.preventDefault();
+
+    // Collect the response
+    let response = form.elements['response'].value;
+
+    if (response % 2 == 0) {
+        alert('Your choice number is even');
+    }
+    if (response % 2 != 0) {
+        alert('Your choice number is odd');
+    }
+
+    // Report the results
+    results.innerHTML = 'You answered ' + response + '.';
+});
+
+//even numbers: remainder of 2 gives u 0, for odd numbers the remainder is not 0. use == 0 at the end
