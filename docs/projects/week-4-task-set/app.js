42cb947f2bb1b9ff48dd31a61761dee394824c9e
Stephanie Zaragoza <szaragoza@college.harvard.edu>
2024-09-24 17:18:15 -0400

    question 3, celsius to farenheit

---
+++ docs/projects/week-4-task-set/app.js
@@ -0,0 +1,11 @@
+function celsiusToFahrenheit(celsius) {
+    let fahrenheit = (celsius * 1.8) + 32;
+    return fahrenheit;
+}
+console.log(celsiusToFahrenheit(10))
+let celsius = celsiusToFahrenheit(10);
+console.log(celsius)
+
+
+
+
