916081f3bba63e0fdec5afdaa2eb7811869ffec6
Stephanie Zaragoza <szaragoza@college.harvard.edu>
2024-09-24 17:42:22 -0400

    question 4 convertTemp

--- docs/projects/week-4-task-set/app.js
+++ docs/projects/week-4-task-set/app.js
@@ -1,11 +1,18 @@
-function celsiusToFahrenheit(celsius) {
-    let fahrenheit = (celsius * 1.8) + 32;
-    return fahrenheit;
+// function celsiusToFahrenheit(celsius) {
+//     let fahrenheit = (celsius * 1.8) + 32;
+//     return fahrenheit;
+// }
+// console.log(celsiusToFahrenheit(10))
+// let celsius = celsiusToFahrenheit(10);
+// console.log(celsius)
+
+function calculateTemperature(temp, convertTo) {
+    if (convertTo == 'c') {
+        return (temp - 32) / 1.8;
+    } else {
+        return (temp * 1.8) + 32;
+    }
 }
-console.log(celsiusToFahrenheit(10))
-let celsius = celsiusToFahrenheit(10);
-console.log(celsius)
-
-
-
+console.log(calculateTemperature(10, 'c'));
+console.log(calculateTemperature(10, 'f'));
 
