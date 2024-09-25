e4f5a8380bc6ad8aeedbb8e231bd49ab09bdc5cc
Stephanie Zaragoza <szaragoza@college.harvard.edu>
2024-09-25 18:11:48 -0400

    getWordLengths answer

--- docs/projects/week-4-task-set/app.js
+++ docs/projects/week-4-task-set/app.js
@@ -6,13 +6,29 @@
 // let celsius = celsiusToFahrenheit(10);
 // console.log(celsius)
 
-function calculateTemperature(temp, convertTo) {
-    if (convertTo == 'c') {
-        return (temp - 32) / 1.8;
-    } else {
-        return (temp * 1.8) + 32;
-    }
-}
-console.log(calculateTemperature(10, 'c'));
-console.log(calculateTemperature(10, 'f'));
+// function calculateTemperature(temp, convertTo) {
+//     if (convertTo == 'c') {
+//         return (temp - 32) / 1.8;
+//     } else {
+//         return (temp * 1.8) + 32;
+//     }
+// }
+// console.log(calculateTemperature(10, 'c'));
+// console.log(calculateTemperature(10, 'f'));
 
+//Question 5
+// let words = ['pink', 'purple', 'green', 'turquoise'];
+
+// for (let word of words) {
+//     console.log(word.length); //Expected Output [4, 6, 5, 9]
+// }
+
+function getWordLengths() {
+    let wordlengths = [];
+    for (let i = 0; i < words.length; i++) {
+        wordlengths.push(words[i].length)
+    }
+    return wordlengths
+}
+let words = ['pink', 'purple', 'green', 'turquoise'];
+console.log(getWordLengths(words));
