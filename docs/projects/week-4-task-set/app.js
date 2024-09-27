5c44a33d5d3d7bff6816a31580210ba7da8224ca
Stephanie Zaragoza <szaragoza@college.harvard.edu>
2024-09-27 11:46:27 -0400

    question 9 even/odd response time task

--- docs/projects/week-4-task-set/app.js
+++ docs/projects/week-4-task-set/app.js
@@ -57,21 +57,47 @@
 // }
 // console.log(getOddNumbers([1, 2, 3, 4, 5]));
 
-function filterNumbers(numbers, evenOrOdd) {
-    let filterNumbers = [];
-    for (let number of numbers) {
-        if (evenOrOdd) {
-            if (number % 2 != 0) {
-                filterNumbers.push(number)
-            }
-        } else
-            if (number % 2 == 0) {
-                filterNumbers.push(number)
-            }
-    }
-    return filterNumbers
-}
+// function filterNumbers(numbers, evenOrOdd) {
+//     let filterNumbers = [];
+//     for (let number of numbers) {
+//         if (evenOrOdd) {
+//             if (number % 2 != 0) {
+//                 filterNumbers.push(number)
+//             }
+//         } else
+//             if (number % 2 == 0) {
+//                 filterNumbers.push(number)
+//             }
+//     }
+//     return filterNumbers
+// }
 
-console.log(filterNumbers([1, 2, 3, 4, 5], 'even')); //Expected output: [2, 4]
-console.log(filterNumbers([1, 2, 3, 4, 5], 'odd')); //Expected output: [1, 3, 5]
+// console.log(filterNumbers([1, 2, 3, 4, 5], 'even')); //Expected output: [2, 4]
+// console.log(filterNumbers([1, 2, 3, 4, 5], 'odd')); //Expected output: [1, 3, 5]
+
+//Question 9
+alert(`Welcome to the even/odd response time task.You are about to see a series of numbers. If the number you see is EVEN, type the letter "e". If the number you see is ODD, type the letter "o." Please answer as quickly and accurately as possible`);
+let results = [];
+for (let i = 0; i < 5; i++) {
+    let num1 = Math.floor(Math.random() * 20) + 1;
+    let start = Date.now();
+    let response = prompt('Number:' + num1 + 'Type the letter "e" for EVEN. Type the letter "o" for ODD');
+    let end = Date.now();
+    let time = (end - start) / 1000; // Calculate response time in seconds
+    let answer = num1
+    let feedback;
+    if (answer % 2 == 0) {
+        feedback = 'true';
+    } else {
+        feedback = 'false';
+    }
+    results.push({
+        Number: answer,
+        response: response,
+        correct: feedback,
+        time: time
+    });
+}
+alert('Thank you for your time.');
+console.log(results);
 
