202b215531aad712d51e6535e28ba877574ae08a
Stephanie Zaragoza <szaragoza@college.harvard.edu>
2024-10-08 17:06:54 -0400

    questionnaire question 3

--- docs/projects/mrt-blocks-jspsch/experiment.js
+++ docs/projects/mrt-blocks-jspsch/experiment.js
@@ -13,128 +13,66 @@ let welcomeTrial = {
     <p>In this experiment, you will be shown a series of math questions.</p>
     <p> There are three parts to this experiment; the questions will increase in difficulty with each part.</p>
     <p>Please answer as quickly and accurately as possible.</p>
-    <p>Press SPACE to begin.</p>
+    <p>Press <span class='key'>SPACE</span> to begin.</p>
     `,
 };
 timeline.push(welcomeTrial);
-//jsPsych.run(timeline);
 
-let firstTrial = {
-    type: jsPsychHtmlKeyboardResponse,
-    stimulus: `
-    <h1>Part One</h1>
+//likert-scale questionnaire
+var likert_scale = [
+    "Strongly Disagree",
+    "Disagree",
+    "Neutral",
+    "Agree",
+    "Strongly Agree"
+];
 
-    <p>Press SPACE to begin.</p>
-    `,
+var questionnaire = {
+    type: jsPsychSurveyLikert,
+    questions: [
+        { prompt: "I enjoy solving math problems.", name: 'prompt1', labels: likert_scale },
+        { prompt: "I find math easy.", name: 'prompt2', labels: likert_scale },
+    ],
+    randomize_question_order: true
 };
-timeline.push(firstTrial);
-//jsPsych.run(timeline);
+timeline.push(questionnaire);
 
 
-//Block One
-blockConditions = jsPsych.randomization.repeat(conditions, 1);
-for (let condition of conditions) {
-    let Trial1 = {
-        type: jsPsychSurveyHtmlForm,
-        preamble: `<h1>What is ${condition.num1} + ${condition.num2}?</h1>`,
-        html: `<p><input type='text' name='answer' id="answer"></p>`,
-        autofocus: 'answer', // id of the field we want to auto-focus on when the trial starts
-        button_label: 'Submit Answer',
-        data: {
-            collect: true,
-            blockId: "Part Two",
-        },
-        on_finish: function (data) {
-            if (data.response.answer === condition.answer) {
-                data.correct = true;
-            } else {
-                data.correct = false;
-            }
-            data.response = data.response.answer
-            data.num1 = condition.num1
-            data.num2 = condition.num2
-            data.answer = condition.answer
-        }
+for (let block of conditions) {
+
+    let blockIntroTrial = {
+        type: jsPsychHtmlKeyboardResponse,
+        stimulus: `
+        <h1>${block.title}</h1>
+        Press space to begin.`,
+        choices: [' ']
     }
-    timeline.push(Trial1);
+    timeline.push(blockIntroTrial);
+
+    for (let question of block.questions) {
+
+        let questionTrial = {
+            type: jsPsychSurveyHtmlForm,
+            preamble: `<p>What is <span class='equation'> ${question.num1}</span> + <span class='equation'>${question.num2}</span>?</p>`,
+            html: `<input name='responseInput' id='response' type='text'>`,
+            autofocus: 'response',
+            button_label: 'Submit Answer',
+            data: {
+                collect: true,
+            },
+            on_finish: function (data) {
+                data.block = block.title;
+                data.response = data.response.responseInput;
+                data.correct = question.answer == data.response;
+                data.num1 = question.num1;
+                data.num2 = question.num2;
+                data.answer = question.answer;
+            }
+        }
+        timeline.push(questionTrial);
+    }
 }
 
-
-let secondTrial = {
-    type: jsPsychHtmlKeyboardResponse,
-    stimulus: `
-    <h1>Part Two</h1>
-
-    <p>Press SPACE to begin.</p>
-    `,
-};
-timeline.push(secondTrial);
-
-//Block Two
-blockConditions = jsPsych.randomization.repeat(conditions, 1);
-for (let condition of conditions) {
-    let Trial2 = {
-        type: jsPsychSurveyHtmlForm,
-        preamble: `<h1>What is ${condition.num1} + ${condition.num2}?</h1>`,
-        html: `<p><input type='text' name='answer' id="answer"></p>`,
-        autofocus: 'answer', // id of the field we want to auto-focus on when the trial starts
-        button_label: 'Submit Answer',
-        data: {
-            collect: true,
-            blockId: "Part Two",
-        },
-        on_finish: function (data) {
-            if (data.response.answer == condition.answer == true) {
-                data.correct = true;
-            } else {
-                data.correct = false;
-            }
-            data.response = data.response.answer
-            data.num1 = condition.num1
-            data.num2 = condition.num2
-            data.answer = condition.answer
-        }
-    }
-    timeline.push(Trial2);
-}
-
-let thirdTrial = {
-    type: jsPsychHtmlKeyboardResponse,
-    stimulus: `
-    <h1>Part Three</h1>
-
-    <p>Press SPACE to begin.</p>
-    `,
-};
-timeline.push(thirdTrial);
-
-//Block Three
-blockConditions = jsPsych.randomization.repeat(conditions, 1);
-for (let condition of conditions) {
-    let Trial1 = {
-        type: jsPsychSurveyHtmlForm,
-        preamble: `<h1>What is ${condition.num1} + ${condition.num2}?</h1>`,
-        html: `<p><input type='text' name='answer' id="answer"></p>`,
-        autofocus: 'answer', // id of the field we want to auto-focus on when the trial starts
-        button_label: 'Submit Answer',
-        data: {
-            collect: true,
-            blockId: "Part Three",
-        },
-        on_finish: function (data) {
-            if (data.response.answer == condition.answer == true) {
-                data.correct = true;
-            } else {
-                data.correct = false;
-            }
-            data.response = data.response.answer
-            data.num1 = condition.num1
-            data.num2 = condition.num2
-            data.answer = condition.answer
-        }
-    }
-    timeline.push(Trial1);
-}
 
 let debriefTrial = {
     type: jsPsychHtmlKeyboardResponse,
