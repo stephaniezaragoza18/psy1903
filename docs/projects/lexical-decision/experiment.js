5b8027b4f8f79fdaedd6dbb372c0296bf6242cc3
Stephanie Zaragoza <szaragoza@college.harvard.edu>
2024-10-21 22:20:33 -0400

    latest

--- docs/projects/lexical-decision/experiment.js
+++ docs/projects/lexical-decision/experiment.js
@@ -1,89 +1,78 @@
 let jsPsych = initJsPsych();
-// Output the resulting conditions array to make sure it is set up correctly
-// console.log(conditions);
 
-// Welcome
 let timeline = [];
 
+// Welcome
 let welcomeTrial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
-    <h1>Welcome to the Math Response Time Task!</h1>
+    <h1>Welcome to the Lexical Decision Task!</h1>
 
-    <p>In this experiment, you will be shown a series of math questions.</p>
-    <p> There are three parts to this experiment; the questions will increase in difficulty with each part.</p>
-    <p>Please answer as quickly and accurately as possible.</p>
-    <p>Press <span class='key'>SPACE</span> to begin.</p>
+    <p>In this experiment, you will be shown a series of characters and asked to categorize whether the characters make up a word or not.</p>
+    <p>There are three parts to this experiment.</p>
+    <p>Press SPACE to begin the first part.</p>
     `,
+    choices: [' ']
 };
+
 timeline.push(welcomeTrial);
 
-//likert-scale questionnaire
-var likert_scale = [
-    "Strongly Disagree",
-    "Disagree",
-    "Neutral",
-    "Agree",
-    "Strongly Agree"
-];
-
-var questionnaire = {
-    type: jsPsychSurveyLikert,
-    questions: [
-        { prompt: "I enjoy solving math problems.", name: 'prompt1', labels: likert_scale },
-        { prompt: "I find math easy.", name: 'prompt2', labels: likert_scale },
-    ],
-    randomize_question_order: true
-};
-timeline.push(questionnaire);
-
-
 for (let block of conditions) {
+
+    let blockConditions = jsPsych.randomization.repeat(block.conditions, 1);
 
     let blockIntroTrial = {
         type: jsPsychHtmlKeyboardResponse,
         stimulus: `
-        <h1>${block.title}</h1>
-        Press space to begin.`,
-        choices: [' ']
-    }
+            <h1>${block.title}</h1>
+            <p>You are about to see a series of ${block.count} characters.</p>
+            <p>If the characters make up a word, press the F key.</p>
+            <p>If the characters do not make up a word, press the J key.</p>
+            <p>Press SPACE to begin.</p>
+            `,
+        choices: [' '],
+    };
     timeline.push(blockIntroTrial);
 
-    for (let question of block.questions) {
-
-        let questionTrial = {
-            type: jsPsychSurveyHtmlForm,
-            preamble: `<p>What is <span class='equation'> ${question.num1}</span> + <span class='equation'>${question.num2}</span>?</p>`,
-            html: `<input name='responseInput' id='response' type='text'>`,
-            autofocus: 'response',
-            button_label: 'Submit Answer',
+    for (let condition of blockConditions) {
+        let conditionTrial = {
+            type: jsPsychHtmlKeyboardResponse,
+            stimulus: `<h1>${condition.characters}</h1>`,
+            choices: ['f', 'j'],
             data: {
                 collect: true,
+                characters: condition.characters,
+                blockId: block.title
             },
             on_finish: function (data) {
-                data.block = block.title;
-                data.response = data.response.responseInput;
-                data.correct = question.answer == data.response;
-                data.num1 = question.num1;
-                data.num2 = question.num2;
-                data.answer = question.answer;
+                if (data.response == 'f' && condition.isWord == true) {
+                    data.correct = true;
+                } else if (data.response == 'j' && condition.isWord == false) {
+                    data.correct = true;
+                } else {
+                    data.correct = false;
+                }
             }
         }
-        timeline.push(questionTrial);
+        timeline.push(conditionTrial);
     }
 }
 
+/**
+ * Results
+ */
 let resultsTrial = {
     type: jsPsychHtmlKeyboardResponse,
     choices: ['NO KEYS'],
     async: false,
     stimulus: `
         <h1>Please wait...</h1>
+        <span class='loader'></span>
         <p>We are saving the results of your inputs.</p>
         `,
     on_start: function () {
         //  ⭐ Update the following three values as appropriate ⭐
-        let prefix = 'mrt';
+        let prefix = 'lexical-decision';
         let dataPipeExperimentId = 'hg4lGx4J7lW1';
         let forceOSFSave = true;
 
@@ -93,6 +82,8 @@ let resultsTrial = {
             .filter({ collect: true })
             .ignore(['stimulus', 'trial_type', 'plugin_version', 'collect'])
             .csv();
+
+        console.log(results);
 
         // Generate a participant ID based on the current timestamp
         let participantId = new Date().toISOString().replace(/T/, '-').replace(/\..+/, '').replace(/:/g, '-');
@@ -125,7 +116,7 @@ let resultsTrial = {
 }
 timeline.push(resultsTrial);
 
-
+// Debrief
 let debriefTrial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
@@ -145,8 +136,3 @@ let debriefTrial = {
 timeline.push(debriefTrial);
 
 jsPsych.run(timeline);
-
-
-
-
-
